f0f4e037334c2c9d5249326b534e0f5820476582
Chaitanya-Sarma-hub <iamsarma030@gmail.com>
2022-11-11 17:14:33 +0530

    2.0

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,47 +1,47 @@
 package com.luv2code.springboot.cruddemo.service;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-	
-	private EmployeeDAO employeeDao;
+
+	private EmployeeRepository employeeRepository;
 
 	@Autowired
-	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
-		this.employeeDao = employeeDAO;
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
 	}
 
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
-		return employeeDao.findAll();
+		return employeeRepository.findAll();
 	}
 
 	@Override
-	@Transactional
 	public Employee findById(int empId) {
-		return employeeDao.findById(empId);
+		Optional<Employee> result = employeeRepository.findById(empId);
+		Employee theEmployee = null;
+		if (result.isPresent()) {
+			theEmployee = result.get();
+		} else {
+			throw new RuntimeException("Did not find employee id: " + empId);
+		}
+		return theEmployee;
 	}
 
 	@Override
-	@Transactional
 	public void save(Employee employee) {
-		employeeDao.save(employee);
+		employeeRepository.save(employee);
 	}
 
 	@Override
-	@Transactional
 	public void deleteById(int empId) {
-		employeeDao.deleteById(empId);
+		employeeRepository.deleteById(empId);
 	}
 
 }
